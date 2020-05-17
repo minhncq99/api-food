cd2bfec81dd8e59bbb60303d1cec0a4d9cf5c52f
Minh <minhncq99@gmail.com>
2020-05-17 08:25:50 +0700

    fix models

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
